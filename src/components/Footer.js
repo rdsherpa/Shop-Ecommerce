7424bb8829d1b318dd188f5b4df98ce1c42fc0ce
Riwa Sherpa <riwasherpa@Riwas-MBP.lan>
2023-04-03 14:03:33 -0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,134 @@
+import { Facebook, Instagram, LinkedIn } from "@mui/icons-material";
+import styled from "styled-components";
+import PlaceIcon from "@mui/icons-material/Place";
+import CallIcon from "@mui/icons-material/Call";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ width: "none" })}
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-item: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>REWA.</Logo>
+        <Desc>
+          If you have any queries please let us know, so that we can accomodate
+          you according to your needs. Thank you.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <LinkedIn />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Men's</ListItem>
+          <ListItem>Woman's</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>WishList</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <PlaceIcon style={{ marginRight: "10px" }} /> Avon, Colorado
+        </ContactItem>
+        <ContactItem>
+          <CallIcon style={{ marginRight: "10px" }} /> +970-390-4845
+        </ContactItem>
+        <ContactItem>
+          <EmailOutlinedIcon style={{ marginRight: "10px" }} />
+          Sherpariwa@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/components/Offer.js
@@ -0,0 +1,18 @@
+import styled, { withTheme } from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Offer = () => {
+  return <Container>Huge Offer! Free Shipping on Orders Over $50</Container>;
+};
+
+export default Offer;
